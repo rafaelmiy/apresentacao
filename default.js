8149f97892924d78f493e600bdf109506b29c797
rafaelmiy <Miyamoto@MacBook-Air-de-Miyamoto.local>
2017-10-29 22:30:16 -0200

    search e ajustes

--- default.js
+++ default.js
@@ -1,10 +1,15 @@
 // Scroll Reveal config
 window.sr = ScrollReveal({ duration: 2000 });
-sr.reveal('.one h2, .two h2', {
+sr.reveal('.one h2, .jobs, .two h2, .three h2, .four h2, .five h2, .six h2, .seven h2, .seven1 h2',{
 	origin: 'top',
 	opacity: 0,
 	reset: true
 });
+// sr.reveal('.search',{
+// 	origin: 'top',
+// 	opacity: 1,
+// 	reset: true
+// });
 
 
 // Scrollify config
@@ -26,3 +31,23 @@ $.scrollify({
     afterResize:function() {},
     afterRender:function() {}
 });
+
+// var input_size = $('#input').val().length;
+
+$( "#input" ).keyup(function() {
+	// if($('#input').val().length >= 6){
+	// 	$('span').show();
+	// 	$('span').addClass("fade");
+	// }
+	var input = $('#input').val();
+	$('#text').html(input);
+
+	$(document).keypress(function(e) {
+		if(e.which == 13) {
+	        // alert('You pressed enter!');
+	        // $(document).scrollTop( $("#8").offset().top );
+	        $('#button')[0].click();
+	    }
+	});
+});
+
