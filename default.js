5e52b57712fe3870e5b16a8b74c63a0eaa92c5f2
rafaelmiy <Miyamoto@MacBook-Air-de-Miyamoto.local>
2017-10-30 00:35:19 -0200

    up

--- default.js
+++ default.js
@@ -30,24 +30,6 @@ $.scrollify({
     after:function() {},
     afterResize:function() {},
     afterRender:function() {}
-},
-{
-section : ".scroll",
-    sectionName : "scroll",
-    interstitialSection : "",
-    easing: "easeOutExpo",
-    scrollSpeed: 1100,
-    offset : 0,
-    scrollbars: true,
-    standardScrollElements: "",
-    setHeights: true,
-    overflowScroll: true,
-    updateHash: true,
-    touchScroll:true,
-    before:function() {},
-    after:function() {},
-    afterResize:function() {},
-    afterRender:function() {}
 });
 // $.scrollify({
 //     section : ".scroll",
